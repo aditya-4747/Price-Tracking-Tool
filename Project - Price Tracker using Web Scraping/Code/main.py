1149b22144c57a110ce97e9c81b8d18f247b881c
aditya-4747 <102402234+aditya-4747@users.noreply.github.com>
2023-02-11 13:50:26 +0530

    Update main.py

--- Project - Price Tracker using Web Scraping/Code/main.py
+++ Project - Price Tracker using Web Scraping/Code/main.py
@@ -4,25 +4,32 @@ import smtplib
 from time import sleep
 import product
 
-head = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
+# My User Agent
+head = ({'User-Agent':'Your user agent should be pasted here. Search for "My user agent" on google to get it.','Accept-Language': 'en-US, en;q=0.5'})
 
+# Taking the URL as input and requesting to the HTTP
 url = input("Enter the link of the Product :")
 scraper = requests.get(url,headers=head)
 
+# Scraping the content using BeautifulSoup
 main = BeautifulSoup(scraper.content,'html5lib')
 
-
+# Functions are called for the price and name, which are stored in the product module.
 true_price = product.price(url,main)
 nameofProduct = str(product.name(url,main))
+
+# Print the name of product and current price
 print("Product Name :",nameofProduct)
 print("Current Price :",true_price)
+
+# Response of the user is taken as input.
 response = input("Do you want to be notified when the price fells down (Specify Yes or No) :")
 if response == "yes" or response == "Yes" :
     print("Thank You!! You'll be notified when the price drops.")
 else :
     exit()
 
-
+# Sending E-mail if current_price is less than the initial price.
 while True :
     curr_price = product.price(url,main)
     if curr_price <= true_price :
