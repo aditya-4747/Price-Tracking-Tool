69e8a6b42c13c384fe4ee0d92d427fe1e48bf709
aditya-4747 <102402234+aditya-4747@users.noreply.github.com>
2023-02-11 13:21:41 +0530

    Add files via upload

---
+++ Project - Price Tracker using Web Scraping/Code/main.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+from time import sleep
+import product
+
+head = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36','Accept-Language': 'en-US, en;q=0.5'})
+
+url = input("Enter the link of the Product :")
+scraper = requests.get(url,headers=head)
+
+main = BeautifulSoup(scraper.content,'html5lib')
+
+
+true_price = product.price(url,main)
+nameofProduct = str(product.name(url,main))
+print("Product Name :",nameofProduct)
+print("Current Price :",true_price)
+response = input("Do you want to be notified when the price fells down (Specify Yes or No) :")
+if response == "yes" or response == "Yes" :
+    email_id = input("Enter your E-mail I'd : ")
+    print("Thank You!! You'll be notified when the price drops.")
+else :
+    exit()
+
+
+while True :
+    curr_price = product.price(url,main)
+    if curr_price <= true_price :
+        server = smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login("adi.kumar0602@gmail.com","atoqypuaecgxgzje")
+        msg = "Hey!!\n\nPrice of " + nameofProduct + ", has fell down by Rs " + str(true_price-curr_price) + ".\n" 
+        link = "Click this link to Purchase it : " + url
+        server.sendmail("adi.kumar0602@gmail.com",email_id,(msg+link))
+        server.quit()
+    sleep(60)  # 60 seconds sleep
+    print("Current Price :",curr_price)
---
+++ Project - Price Tracker using Web Scraping/Code/product.py
@@ -0,0 +1,41 @@
+def price(url,main) :
+    az = url.find('amazon')
+    fk = url.find('flipkart')
+    sd = url.find('snapdeal')
+    if url[fk:(fk+4)] == 'flip' :
+        price = main.find("div",class_="_30jeq3 _16Jk6d")
+    elif url[sd:(sd+4)] == 'snap' :
+        price = main.find("span",class_="payBlkBig")
+    elif url[az:(az+4)] == 'amaz' :
+        price = main.find("span",class_="a-price-whole")
+        for item in price.strings :
+            price = item
+            break
+    else :
+        print("This is not a valid Link of either Amazon/Flipkart/Snapdeal.")
+        exit()
+    num_price = int((repr(price.string)).replace("₹","").replace(",","").replace("'",""))
+    return num_price
+
+
+def name(url,main) :
+    fk = url.find('flipkart')
+    sd = url.find('snapdeal')
+    if url[fk:(fk+4)] == 'flip' :
+        name = main.find("span",class_="B_NuCI")
+        for item in name.stripped_strings :
+            name = item
+            break
+        return name
+    elif url[sd:(sd+4)] == 'snap' :
+        name = main.find("h1",class_="pdp-e-i-head")
+        for item in name.stripped_strings :
+            name = item
+            break
+        return name
+    else :
+        name = main.find("span",id="productTitle")
+        for item in name.stripped_strings :
+            name = item
+            break
+        return name
